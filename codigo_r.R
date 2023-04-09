041d19fca3b66c0f79555387302828e2dfcc718c
Luiza Moura <luizamoura90@gmail.com>
2023-04-09 13:43:00 -0300

    Novas alterções de INSE

--- codigo_r.R
+++ codigo_r.R
@@ -12,7 +12,7 @@ View(geolocalizacao_escolas)
 
 #FONTE:  https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/indicadores-educacionais/nivel-socioeconomico
 INSE_2019_ESCOLAS <- read_excel("data/INSE_2019_ESCOLAS.xlsx", 
-                                     col_types = c("numeric", "text", "numeric", 
+                                     col_types = c("text", "text", "numeric", 
                                                              "text", "numeric", "text", "numeric", 
                                                              "numeric", "numeric", "numeric", 
                                                              "numeric", "text", "numeric", "numeric", 
@@ -36,32 +36,70 @@ library(tidyr)
 
 
 
+#geolocalizacao_escolas %>%
+  #drop_na(Latitude, Longitude)%>%
+#  leaflet() %>% 
+#  addTiles() %>% 
+#  addProviderTiles(providers$OpenStreetMap.DE) %>% 
+#  setView(-38.4368023,-12.9144042,10) %>%
+#  addMarkers(lng=~Longitude,
+#             lat=~Latitude, popup =~UF)
+
 geolocalizacao_escolas %>%
-  #drop_na(Latitude, Longitude)%>%
-  leaflet() %>% 
-  addTiles() %>% 
-  addProviderTiles(providers$OpenStreetMap.DE) %>% 
+  drop_na(Latitude, Longitude)%>%
+  leaflet() %>%
+  addTiles() %>%
+  addProviderTiles(providers$OpenStreetMap.DE) %>%
   setView(-38.4368023,-12.9144042,10) %>%
-  addMarkers(lng=~Longitude,
-             lat=~Latitude, popup =~UF)
+  addCircles(lng=~Longitude,
+             lat=~Latitude, popup = ~`Porte da Escola`)
+###############################################################################
+# Filtro para Salvador
+INSE_2019_ESCOLAS_SSA<- INSE_2019_ESCOLAS %>% filter(CO_MUNICIPIO %in% c("2927408"))
 
-names(geolocalizacao_escolas)[3] <- c("codigo_inep")
-names(mapeamento_salvador)[18] <- c("codigo_inep")
-names(INSE_2019_ESCOLAS)[1] <- c("codigo_inep")
+library(magrittr)
+library(htmltools)
 
-mapeamento_salvador_localizacao <- mapeamento_salvador |>
-  inner_join(geolocalizacao_escolas) 
-View(mapeamento_salvador_localizacao)
+# Conversao numeric to character
+geolocalizacao_escolas %<>% mutate(`Código INEP` = as.character(`Código INEP`))
 
-mapeamento_salvador_localizacao %>%
-  #drop_na(Latitude, Longitude)%>%
-  leaflet() %>% 
-  addTiles() %>% 
-  addProviderTiles(providers$OpenStreetMap.DE) %>% 
+INSE_GEO_SSA<- full_join(x = INSE_2019_ESCOLAS_SSA,
+                         y = geolocalizacao_escolas,
+                         by = c("CO_ESCOLA"="Código INEP"))
+
+INSE_GEO_SSA %<>% mutate(INSE_CLASSIFICACAO =as.factor(INSE_CLASSIFICACAO))
+factpal <- colorFactor(topo.colors(5), as.factor(INSE_GEO_SSA$INSE_CLASSIFICACAO))
+
+INSE_GEO_SSA %>%
+  drop_na(Latitude, Longitude)%>%
+  leaflet() %>%
+  addTiles() %>%
+  addProviderTiles(providers$OpenStreetMap.DE) %>%
   setView(-38.4368023,-12.9144042,10) %>%
-  addMarkers(lng=~Longitude,
-             lat=~Latitude, popup =~UF)
+  addCircles(lng=~Longitude,
+             lat=~Latitude,
+             radius = ~QTD_ALUNOS_INSE,
+             color = ~factpal(INSE_CLASSIFICACAO),
+             label = ~htmlEscape(NOME_ESCOLA))
 
-escolas_desempenho <- mapeamento_salvador |>
-  inner_join(INSE_2019_ESCOLAS) 
-View(escolas_desempenho)
+#names(geolocalizacao_escolas)[3] <- c("codigo_inep")
+#names(mapeamento_salvador)[18] <- c("codigo_inep")
+#names(INSE_2019_ESCOLAS)[1] <- c("codigo_inep")
+
+#mapeamento_salvador_localizacao <- mapeamento_salvador |>
+#  inner_join(geolocalizacao_escolas) 
+#View(mapeamento_salvador_localizacao)
+
+#mapeamento_salvador_localizacao %>%
+  #drop_na(Latitude, Longitude)%>%
+ # leaflet() %>% 
+  #addTiles() %>% 
+  #addProviderTiles(providers$OpenStreetMap.DE) %>% 
+  #setView(-38.4368023,-12.9144042,10) %>%
+  #addMarkers(lng=~Longitude,
+   #          lat=~Latitude, popup =~UF)
+
+
+#escolas_desempenho <- mapeamento_salvador |>
+#  inner_join(INSE_2019_ESCOLAS) 
+#View(escolas_desempenho)
